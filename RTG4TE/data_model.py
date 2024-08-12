7e8437a89f57ae3e0e10cf16bd24653527caad89
luwry <39986145+luwry@users.noreply.github.com>
2024-08-13 00:40:43 +0800

    Delete RTG4TE directory

--- RTG4TE/data_model.py
+++
@@ -1,8 +0,0 @@
-# -*-coding:utf-8-*-
-import argparse
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--train-file',type=str, default='data/ree_train.json')
-    parser.add_argument('--val-file', type=str, default='data/ree_dev.json')
-    parser.add_argument('--test-file', type=str, default='data/ree_test.json')
-    parser.add_argument('--tmp_dir', default='./tmp')
